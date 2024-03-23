b88c5f1b28945299766ea8fb017ff4deb844d574
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-03-23 14:23:52 +0100

    4th commit

--- VTUProject/settings.py
+++ VTUProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*5mh&qq*p9uz+89&ojp**zd398ead(e)b5u#y^3xdt3ccb2i48
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vtuapis.onrender.com', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
